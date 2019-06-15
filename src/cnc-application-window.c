ccd34bb58aa37b2673060b4a0988043fc3f996f0
Robert Ancell <robert.ancell@gmail.com>
2019-06-15 23:06:13 +1200

    Get some communication working with GRBL

--- src/cnc-application-window.c
+++ src/cnc-application-window.c
@@ -8,6 +8,7 @@
  */
 
 #include "cnc-application-window.h"
+#include "cnc-grbl-client.h"
 #include "cnc-plan.h"
 #include "cnc-plan-view.h"
 
@@ -18,17 +19,28 @@ struct _CncApplicationWindow
     CncPlanView *plan_view;
 
     CncPlan *plan;
+
+    CncGrblClient *grbl_client;
 };
 
 G_DEFINE_TYPE (CncApplicationWindow, cnc_application_window, GTK_TYPE_APPLICATION_WINDOW)
 
 static void
+cnc_application_window_dispose (GObject *object)
+{
+    CncApplicationWindow *self = CNC_APPLICATION_WINDOW (object);
+    g_clear_object (&self->plan);
+    g_clear_object (&self->grbl_client);
+}
+
+static void
 cnc_application_window_class_init (CncApplicationWindowClass *klass)
 {
-    GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
-    gtk_widget_class_set_template_from_resource (widget_class, "/com/example/CncDesigner/cnc-application-window.ui");
+    G_OBJECT_CLASS (klass)->dispose = cnc_application_window_dispose;
 
-    gtk_widget_class_bind_template_child (widget_class, CncApplicationWindow, plan_view);
+    gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass), "/com/example/CncDesigner/cnc-application-window.ui");
+
+    gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (klass), CncApplicationWindow, plan_view);
 }
 
 static void
@@ -49,6 +61,10 @@ cnc_application_window_init (CncApplicationWindow *self)
         cnc_plan_add_layer (self->plan);
     }
     cnc_plan_view_set_plan (self->plan_view, self->plan);
+
+    self->grbl_client = cnc_grbl_client_new ("/dev/ttyUSB0");
+    cnc_grbl_client_connect (self->grbl_client, NULL, NULL);
+    //cnc_grbl_client_send_command (self->grbl_client, "$\r\n");
 }
 
 CncApplicationWindow *
--- src/cnc-grbl-client.c
+++ src/cnc-grbl-client.c
@@ -7,6 +7,12 @@
  * (at your option) any later version.
  */
 
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+
 #include "cnc-grbl-client.h"
 
 struct _CncGrblClient
@@ -14,8 +20,9 @@ struct _CncGrblClient
     GObject parent_instance;
 
     GCancellable *cancellable;
-    GFile *file;
-    GFileIOStream *stream;
+    gchar *path;
+    GIOChannel *channel;
+    GSource *read_source;
 
     gchar *version;
 };
@@ -28,7 +35,9 @@ cnc_grbl_client_dispose (GObject *object)
     CncGrblClient *self = CNC_GRBL_CLIENT (object);
     g_cancellable_cancel (self->cancellable);
     g_clear_object (&self->cancellable);
-    g_clear_object (&self->file);
+    g_clear_pointer (&self->path, g_free);
+    g_clear_pointer (&self->channel, g_io_channel_unref);
+    g_clear_pointer (&self->read_source, g_source_unref);
     g_clear_pointer (&self->version, g_free);
 }
 
@@ -48,47 +57,83 @@ CncGrblClient *
 cnc_grbl_client_new (const gchar *path)
 {
     CncGrblClient *self = g_object_new (cnc_grbl_client_get_type (), NULL);
-    self->file = g_file_new_for_path (path);
+    self->path = g_strdup (path);
     return self;
 }
 
-static void read_cb (GObject *object, GAsyncResult *result, gpointer user_data);
-
-static void
-read_from_controller (CncGrblClient *self)
-{
-    GInputStream *input_stream = g_io_stream_get_input_stream (G_IO_STREAM (self->stream));
-    g_input_stream_read_bytes_async (input_stream, 1024, G_PRIORITY_DEFAULT, self->cancellable, read_cb, self);
-}
-
-static void
-read_cb (GObject *object, GAsyncResult *result, gpointer user_data)
+static gboolean
+read_cb (GIOChannel *channel, GIOCondition condition, gpointer user_data)
 {
     CncGrblClient *self = user_data;
 
+    gchar data[1024];
+    gsize n_read;
     g_autoptr(GError) error = NULL;
-    g_autoptr(GBytes) data = g_input_stream_read_bytes_finish (G_INPUT_STREAM (object), result, &error);
-    if (data == NULL) {
-        if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
-            return;
+    GIOStatus status = g_io_channel_read_chars (self->channel, data, 1024, &n_read, &error);
+    if (status == G_IO_STATUS_ERROR) {
+        g_warning ("Failed to read from GRBL controller: %s", error->message);
+        return G_SOURCE_REMOVE;
     }
 
-    // FIXME: Parse responses
+    if (status == G_IO_STATUS_EOF) {
+        g_warning ("GRBL controller closed connection");
+        return G_SOURCE_REMOVE;
+    }
 
-    read_from_controller (self);
+    if (status != G_IO_STATUS_NORMAL)
+        return G_SOURCE_CONTINUE;
+
+    // FIXME: Parse responses
+    g_printerr ("read '");
+    for (gsize i = 0; i < n_read; i++) {
+        gchar c = data[i];
+        if (c == '\n')
+            g_printerr ("\\n");
+        else if (c == '\r')
+            g_printerr ("\\r");
+        else if (c == '\\')
+            g_printerr ("\\");
+        else
+            g_printerr ("%c", c);
+    }
+    g_printerr ("'\n");
+
+    return G_SOURCE_CONTINUE;
 }
 
 gboolean
 cnc_grbl_client_connect (CncGrblClient *self, GCancellable *cancellable, GError **error)
 {
     g_return_val_if_fail (CNC_IS_GRBL_CLIENT (self), FALSE);
-    g_return_val_if_fail (self->stream == NULL, FALSE);
+    g_return_val_if_fail (self->channel == NULL, FALSE);
 
-    self->stream = g_file_open_readwrite (self->file, cancellable, error);
-    if (self->stream == NULL)
-        return FALSE;
+    int fd = open (self->path, O_RDWR | O_NOCTTY | O_NDELAY);
 
-    read_from_controller (self);
+    /* Configure port */
+    struct termios termios;
+    tcgetattr (fd, &termios);
+    cfsetispeed (&termios, B115200);
+    cfsetospeed (&termios, B115200);
+    termios.c_cflag &= ~CSIZE;
+    termios.c_cflag |= CS8;
+    termios.c_cflag |= CLOCAL | CREAD;
+    termios.c_cflag &= ~(PARENB | PARODD);
+    termios.c_cflag &= ~CSTOPB;
+    termios.c_iflag = IGNBRK;
+    termios.c_lflag = ECHONL | NOFLSH;
+    termios.c_oflag = 0;
+    tcsetattr (fd, TCSANOW, &termios);
+    tcflush (fd, TCOFLUSH);
+    tcflush (fd, TCIFLUSH);
+
+    self->channel = g_io_channel_unix_new (fd);
+    g_io_channel_set_close_on_unref (self->channel, TRUE);
+    self->read_source = g_io_create_watch (self->channel, G_IO_IN);
+    g_source_set_callback (self->read_source, G_SOURCE_FUNC (read_cb), self, NULL);
+    GMainContext *context = g_main_context_get_thread_default ();
+    if (context == NULL)
+        context = g_main_context_default ();
+    g_source_attach (self->read_source, context);
 
     return TRUE;
 }
@@ -105,9 +150,13 @@ cnc_grbl_client_send_command (CncGrblClient *self, const gchar *command)
 {
     g_return_if_fail (CNC_IS_GRBL_CLIENT (self));
     g_return_if_fail (command != NULL);
+    g_return_if_fail (self->channel != NULL);
 
-    GOutputStream *output_stream = g_io_stream_get_output_stream (G_IO_STREAM (self->stream));
     g_autoptr(GError) error = NULL;
-    g_autoptr(GBytes) data = g_bytes_new_static (command, strlen (command));
-    g_output_stream_write_bytes (G_OUTPUT_STREAM (output_stream), data, self->cancellable, &error);
+    gsize command_length = strlen (command);
+    gsize n_written;
+    GIOStatus status = g_io_channel_write_chars (self->channel, command, command_length, &n_written, &error); // FIXME: handle short writes
+    if (status == G_IO_STATUS_ERROR)
+        g_warning ("Failed to write to GRBL controller: %s", error->message);
+    g_assert_cmpint (n_written, ==, command_length);
 }
